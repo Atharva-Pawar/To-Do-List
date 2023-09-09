324591392f1158b582b580bcbc64cd2995c45d49
Atharva <atharvapawar846@gamil.com>
2023-09-09 10:14:30 +0530

    UpdateJS

--- script.js
+++ script.js
@@ -1,21 +1,46 @@
 const inputValue = document.querySelector("input");
 const addBtn = document.querySelector(".btn-add");
 const ulList = document.querySelector("ul");
+const itemList = []; // Initialize the array to store items
+
+// Load items from local storage when the page loads
+window.addEventListener('load', () => {
+    const storedItems = JSON.parse(localStorage.getItem("items")) || [];
+    itemList.push(...storedItems);
+    renderItems(); // Call the render function
+});
+
+// Function to render the list items
+function renderItems() {
+    ulList.innerHTML = ""; // Clear the list first
+
+    itemList.forEach((itemText, index) => {
+        const createLI = document.createElement("li");
+        createLI.innerHTML = `${itemText}<button class="btn-remove" data-index="${index}">Remove</button>`;
+        ulList.appendChild(createLI);
+
+        const removeBtn = createLI.querySelector(".btn-remove");
+        removeBtn.addEventListener('click', (event) => {
+            const indexToRemove = event.target.getAttribute("data-index");
+            itemList.splice(indexToRemove, 1);
+            renderItems(); // Re-render the list
+            updateLocalStorage(); // Update local storage
+        });
+    });
+}
+
+// Update local storage with the current list of items
+function updateLocalStorage() {
+    localStorage.setItem("items", JSON.stringify(itemList));
+}
 
 addBtn.addEventListener('click', () => {
     const inputText = inputValue.value.trim();
 
     if (inputText !== "") {
-        const createLI = document.createElement("li");
-        createLI.innerHTML = `${inputText}<button class="btn-remove">Remove</button>`
-        ulList.appendChild(createLI);
-
-        inputValue.value = "";
-
-        const removeBtn = createLI.querySelector(".btn-remove")
-
-        removeBtn.addEventListener('click', () => {
-            ulList.removeChild(createLI);
-        });
+        itemList.push(inputText); // Add item to the array
+        renderItems(); // Re-render the list
+        updateLocalStorage(); // Update local storage
+        inputValue.value = ""; // Clear input field
     }
-});        
+});
